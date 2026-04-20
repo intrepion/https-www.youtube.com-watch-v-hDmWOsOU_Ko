fc06c80716c3435efb7891a0d863aaa0a79dc28b
Oliver Forral <intrepion@gmail.com>
2026-04-19 20:32:56 -0600

    2D Flutter logo that can be rotated.

--- workspace/lib/main.dart
+++ workspace/lib/main.dart
@@ -99,33 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ..rotateX(_offset.dy / 100)
             ..rotateY(_offset.dx / 100),
           alignment: Alignment.center,
-          child: Center(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
-            child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
-              //
-              // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-              // action in the IDE, or press "p" in the console), to see the
-              // wireframe for each widget.
-              mainAxisAlignment: .center,
-              children: [
-                const Text('You have pushed the button this many times:'),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
-          ),
+          child: Center(child: Cube()),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: _incrementCounter,
@@ -134,5 +108,14 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+}
+
+class Cube extends StatelessWidget {
+  const Cube({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogo(size: 100);
   }
 }
